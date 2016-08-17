b5e3b3eea9c2c9bdb0eaa67acea1710bc808b12c
Reinis Riekstins <riekstins.reinis@gmail.com>
2016-08-17 16:24:14 +0300

    i'm stupid...

---
+++ src/containers/book_list.js
@@ -0,0 +1,20 @@
+"use strict";
+import React, { Component } from 'react';
+
+export default class BookList extends Component {
+  renderList() {
+    return this.props.books.map(book => {
+      return (
+        <li key={book.title} className="list-group-item">{book.title}</li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.renderList()}
+      </ul>
+    );
+  }
+}
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+"use strict";
+export default function () {
+  return [
+    { title: 'jQuery' },
+    { title: 'Angular'},
+    { title: 'React'  },
+    { title: 'Ember'  }
+  ];
+}
