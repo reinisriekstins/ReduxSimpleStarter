a943f048ed1d1c5881aeb9990ddd3b39891d377d
Reinis Riekstins <riekstins.reinis@gmail.com>
2017-02-27 13:41:40 +0200

    cleaned up video_detail component

--- src/components/video_detail.jsx
+++ src/components/video_detail.jsx
@@ -1,51 +1,21 @@
 'use strict';
 import React, { Component } from 'react';
 
-
-//class VideoDetail extends Component {
-//  constructor(props) {
-//    super(props);
-//
-//    this.state = {
-//      videoId: this.props.video.id.videoId,
-//      url: `https://www.youtube.com/embed/${this.state.videoId}`
-//    };
-//  }
-//
-//  render() {
-//    if (!this.props.video) {
-//      return ( <div>Loading...</div> );
-//    }
-//    else {
-//      return (
-//        <div className="video-detail col-md-8">
-//          <div className="embed-responsive embed-responsive-16by9">
-//            <iframe src={this.state.url} frameborder="0" className="embed-responsive-item"></iframe>
-//          </div>
-//
-//          <div className="details">
-//            <div>{this.props.video.snippet.title}</div>
-//            <div>{this.props.video.snippet.description}</div>
-//          </div>
-//        </div>
-//      );
-//    }
-//  }
-//}
-
-
-const VideoDetail = ({video}) => {
+export default ({video}) => {
   if (!video) {
     return ( <div>Loading...</div> );
   }
 
-  const videoId = video.id.videoId;
-  const url = `https://www.youtube.com/embed/${videoId}`;
+  const { videoId } = video.id;
 
   return (
     <div className="video-detail col-md-8">
       <div className="embed-responsive embed-responsive-16by9">
-        <iframe src={url} frameborder="0" className="embed-responsive-item"></iframe>
+        <iframe
+          src={`https://www.youtube.com/embed/${videoId}`}
+          frameborder="0"
+          className="embed-responsive-item">
+        </iframe>
       </div>
 
       <div className="details">
@@ -55,5 +25,3 @@ const VideoDetail = ({video}) => {
     </div>
   );
 }
-
-export default VideoDetail;
